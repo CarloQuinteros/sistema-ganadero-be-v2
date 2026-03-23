53e62eecdb0e092a05c9156850906b2f7398192b
Carlo Quinteros <carloq24@gmail.com>
2026-03-23 11:21:14 -0400

    Feat: adicion rutas de autenticacion

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
 import agendaRouter from "./routes/agendaRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -9,6 +13,7 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/agenda", agendaRouter);
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
