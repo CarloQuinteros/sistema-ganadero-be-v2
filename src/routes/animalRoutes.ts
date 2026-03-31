4f670e6bbaeb032539f8705916a3e045c81d7fe9
Carlo Quinteros <carloq24@gmail.com>
2026-03-31 14:50:52 -0400

    Feat: Agregar endpoints CRUD

--- src/routes/animalRoutes.ts
+++ src/routes/animalRoutes.ts
@@ -3,13 +3,33 @@ import { authorization } from "../middleware/authMiddleware.js";
 import { authorizeRole } from "../middleware/roleMiddleware.js";
 import { validateImage } from "../middleware/validateImageMiddleware.js";
 import upload from "../middleware/animalUploadMiddleware.js";
-import { uploadAnimalImage } from "../controllers/animalController.js";
+import {
+  createAnimalSchema,
+  updateAnimalSchema,
+} from "../schemas/animalSchema.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import {
+  uploadAnimalImage,
+  createAnimal,
+  updateAnimal,
+  deleteAnimal,
+  getAnimalById,
+  getAnimalByEarTag,
+  getAnimals,
+} from "../controllers/animalController.js";
 
 const animalRouter = Router();
 
-animalRouter.get("/", authorization, authorizeRole("ADMIN"), (req, res) => {
-  res.json({ message: "Animal route is working!" });
-});
+animalRouter.get("/", authorization, authorizeRole("ADMIN"), getAnimals);
+
+animalRouter.post(
+  "/",
+  authorization,
+  authorizeRole("ADMIN"),
+  upload.single("image"),
+  validateSchema(createAnimalSchema),
+  createAnimal,
+);
 
 animalRouter.post(
   "/upload",
@@ -18,6 +38,29 @@ animalRouter.post(
   upload.single("image"),
   validateImage,
   uploadAnimalImage,
+);
+
+animalRouter.put(
+  "/:id",
+  authorization,
+  authorizeRole("ADMIN"),
+  validateSchema(updateAnimalSchema),
+  updateAnimal,
+);
+
+animalRouter.delete(
+  "/:id",
+  authorization,
+  authorizeRole("ADMIN"),
+  deleteAnimal,
+);
+
+animalRouter.get("/:id", authorization, authorizeRole("ADMIN"), getAnimalById);
+animalRouter.get(
+  "/eartag/:eartag",
+  authorization,
+  authorizeRole("ADMIN"),
+  getAnimalByEarTag,
 );
 
 export default animalRouter;
--- src/routes/corralRoutes.ts
+++ src/routes/corralRoutes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 import { authorization } from "../middleware/authMiddleware.js";
 import { authorizeRole } from "../middleware/roleMiddleware.js";
+import { createCorralSchema } from "../schemas/corralSchema.js";
+import { validateSchema } from "../middleware/validateSchema.js";
+import { createCorral } from "../controllers/corralController.js";
 
 const corralRouter = Router();
 
 corralRouter.get("/", authorization, authorizeRole("ADMIN"), (req, res) => {
   res.json({ message: "Corral route is working!" });
 });
+
+corralRouter.post(
+  "/",
+  authorization,
+  authorizeRole("ADMIN"),
+  validateSchema(createCorralSchema),
+  createCorral,
+);
 
 export default corralRouter;
