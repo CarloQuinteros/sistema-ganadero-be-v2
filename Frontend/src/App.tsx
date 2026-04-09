db793d7bb36af375823753e95460c61b3e644bfb
Carlo Quinteros <carloq24@gmail.com>
2026-04-09 11:34:18 -0400

    Rutas y creacion de componentes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,8 @@
 import "./index.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return (
-    <>
-      <div className="bg-black  min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl text-blue-400">Tailwind funcionando</h1>
-      </div>
-      <div className="bg-red-500 text-green-900 min-h-screen flex items-center justify-center">
-        YA FUNCIONA 🔥
-      </div>
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
